cc222eb801d47d2820ab28682f7a573aa257ccaa
Faludi Zoltán <zoltan@faludi.info>
2025-12-31 22:18:55 +0100

    Update litepad_install.sh

--- install/litepad_install.sh
+++ install/litepad_install.sh
@@ -9,6 +9,5 @@ cp -u ../source/litepad_96.png litepad
 cp -u ../LICENSE litepad
 cp -u ../README.md litepad
 
-tar -czvf ./linux/litepad_25.12.0.7_amd64.tar.gz litepad
+tar -czvf ./linux/litepad_25.12.0.8_amd64.tar.gz litepad
 rm -r -f litepad
-
